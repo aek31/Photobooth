427c25913b33771ddf427eea083b7e16285fcc74
aek31 <42246569+aek31@users.noreply.github.com>
2018-08-17 21:30:24 +0200

    Update photobooth.py

--- photobooth.py
+++ photobooth.py
@@ -5,10 +5,8 @@ import pygame
 from gpiozero import Button
 import os, sys, time
 
-
 # ATTENTION : CLE USB A FORMATTER EN EXT4 + créer dossier "Photobooth" dans la racine du dossier (il faut 
 # media/pi/cle_photos/Photobooth/ pour que ca fonctionne ou sinon renomer cette ligne pour que ca corresponde à la clé USB
-
 
 button = Button(17)  # Bouton relie au GPIO 17
 Numeral = ""
@@ -22,7 +20,6 @@ background = background.convert()
 
 # rend invisible le curseur de souris
 pygame.mouse.set_visible(0)
-
 
 def UpdateDisplay_vide():
     global Numeral # variable de chiffre de decompte
@@ -39,29 +36,23 @@ def UpdateDisplay_vide():
     pygame.display.flip()
     return
 
-
 def UpdateDisplay_decompteur():
     global Numeral # variable de chiffre de decompte
     
     # Reglage transparance pour afficher le 3,2,1
     # Transparency allows pigame to shine through ok 120
     camera.preview_alpha = 190
-        
     background.fill(pygame.Color("black"))
 
     # initialisation du chiffre 3,2,1
     font = pygame.font.Font(None, 1200)
     font_surf = font.render(Numeral, 1, (62,246,19))
-    #font_surf = font.render(Numeral, 1, (255,0,0))
     font_rect = font_surf.get_rect()
     font_rect.left = 600
     font_rect.top = 0
     background.blit(font_surf, font_rect)
     screen.blit(background, (0,0))
-    #carre qui entoure le nombre
-    #pygame.draw.rect(screen,pygame.Color("green"),(10,10,140,200),2) 
     pygame.display.flip()
-
     return
 
 def UpdateCamera():              # parametre affichage 
@@ -71,12 +62,7 @@ def UpdateCamera():              # parametre affichage
 
     #luminosite a garder a 50, si plus pose probleme avec lumiere
     camera.brightness = 50
-
-    # mode auto d'exposition, avec lumiere ca ne change pas grand chose
-    #camera.exposure_mode = 'auto'
-    #camera.contrast = 8
     camera.resolution = (1640,922)
-
     return
 
 def CanevasPhoto():
@@ -93,12 +79,10 @@ def CanevasPhoto():
     pygame.draw.rect(screen, (62, 246, 19), pygame.Rect(0, 918, 300, 5))
     pygame.draw.rect(screen, (62, 246, 19), pygame.Rect(0, 918, 5, -300))
 
-    
     #bas droit
     pygame.draw.rect(screen, (62, 246, 19), pygame.Rect(1635, 918, -300, 5))
     pygame.draw.rect(screen, (62, 246, 19), pygame.Rect(1635, 921, 5, -300))
 
-    
     #OVERLAY CAPTIONS AS TEXT
     text = "Chic Guinguette Chic - 30 ans"
     font = pygame.font.Font(None, 60)
@@ -110,31 +94,9 @@ def CanevasPhoto():
     pygame.display.update()
 
     sleep(5)
-    
     return
 
-
-
-
 while(True):
-
-    # Verification que la cle USB est montee et creation du dossier Photobooth sur la cle
-    #usbcheck = False
-    #rootdir = '/media/'
-
-    #while not usbcheck:
-    #    dirs = os.listdir(rootdir)
-    #    for file in dirs:
-    #        folder = os.path.join(rootdir,file)
-    #        if not file == 'SETTINGS' and os.path.isdir(folder):
-    #            usbcheck = True
-    #            imagedrive = os.path.join(rootdir,file)
-    #            imagefolder = os.path.join(imagedrive,'PhotoBooth')
-                #If a photobooth folder on the usb doesn't exist create it
-    #            if not os.path.isdir(imagefolder):
-    #                os.makedirs(imagefolder)
-
-    
         
     #Initialise the camera object
     camera = PiCamera()
@@ -162,35 +124,24 @@ while(True):
                 
             UpdateDisplay_vide()
 
-
-
-            
-            # variale vaut la date du jour
+            # varialbe vaut la date du jour
             fname = (time.strftime("%d%b%y_%H:%M:%S"))
             variable = fname + ".jpg"
             
             # enregistrement de l'image
-            #camera.capture(os.path.join(imagefolder,variable))
             camera.capture(os.path.join('/media/pi/cle_photos/Photobooth/',variable))
             camera.stop_preview()
             
-
-            # ICI faire un updatedisplay vide sans texte et bord pour effecer le "1" et carre  qui restent trop longtemps
-            #UpdateDisplay_vide()
-    
             # ecran noir
             img = pygame.image.load('/home/pi/Documents/Tests/noir.jpg')
             screen.blit(img, (0, 0))
             pygame.display.flip()    # update l'affiche de la photo
             sleep(0.3)
-
-            # ici mettre code led flash en meme temps que l'ecran noir
     
             # affiche pendant 5 secondes l'image
             img = pygame.image.load(os.path.join('/media/pi/cle_photos/Photobooth/',variable))
             screen.blit(img, (0, 0))
 
-            
             # Incruste des textes et dessins sur la photo affichee
             CanevasPhoto()
 
